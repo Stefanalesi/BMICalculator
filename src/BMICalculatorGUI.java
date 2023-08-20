1d066115dc8223fb786fa7e75fcf3fb0844ff372
user <user@DESKTOP-UDGVFOI>
2023-08-20 17:21:15 +0200

    idk what i did i forgot i did it yesterday and forgot to commit

--- src/BMICalculatorGUI.java
+++ src/BMICalculatorGUI.java
@@ -4,10 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BMICalculatorGUI {
-    private JFrame frame;
-    private JTextField weightField;
-    private JTextField heightField;
-    private JTextArea resultArea;
+    private final JFrame frame;
+    private final JTextField weightField;
+    private final JTextField heightField;
 
     public BMICalculatorGUI() {
         frame = new JFrame("BMI Calculator");
@@ -20,7 +19,8 @@ public class BMICalculatorGUI {
         JLabel heightLabel = new JLabel("Height (m):");
         heightField = new JTextField();
 
-        JButton calculateButton = new JButton("Calculate");
+        JButton calculateButton = new JButton("Calculate BMI");
+
         calculateButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 double weight = Double.parseDouble(weightField.getText());
@@ -30,12 +30,27 @@ public class BMICalculatorGUI {
                 double bmi = calculator.calculateBMI(weight, height);
                 String interpretation = calculator.interpretBMI(bmi);
 
-                resultArea.setText(String.format("BMI: %.2f\n%s", bmi, interpretation));
+                frame.dispose();
+
+                JFrame resultFrame = new JFrame("BMI Result");
+                resultFrame.setLayout(new BorderLayout());
+                resultFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                JTextArea resultTextArea = new JTextArea();
+                resultTextArea.setText(String.format("BMI: %.2f\n%s", bmi, interpretation));
+
+                Font font = new Font("Arial", Font.PLAIN, 26);
+                resultTextArea.setFont(font);
+
+                resultTextArea.setEditable(false);
+
+                resultFrame.add(new JScrollPane(resultTextArea), BorderLayout.CENTER);
+
+                resultFrame.setSize(200, 150);
+
+                resultFrame.setVisible(true);
             }
         });
-
-        resultArea = new JTextArea();
-        resultArea.setEditable(false);
 
         frame.add(weightLabel);
         frame.add(weightField);
@@ -43,7 +58,6 @@ public class BMICalculatorGUI {
         frame.add(heightField);
         frame.add(new JLabel());
         frame.add(calculateButton);
-        frame.add(resultArea);
 
         frame.pack();
         frame.setVisible(true);
